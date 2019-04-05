137bf3ccab07e3966c56cddc3d5b8daeb98c6682
Piotr Tarasiuk <piotr.tarasiuk@softwarehut.com>
2019-04-05 17:33:33 +0200

    Solutions for lab09 missing save to file

--- lab09/04/app.js
+++ lab09/04/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const fs = require('fs');
 const app = express();
 
 const words = ['piwo', 'hazard', 'disco-polo', 'cukierki'];
@@ -12,7 +12,13 @@ app.post('/comment', (req, res) => {
     if (hasBadWord) {
         return res.status(400).send('comment has bad word');
     }
+    fs.writeFileSync('./comment.txt', req.body);
     res.send('ok');
+});
+
+app.get('/comment', (req, res) => {
+    const comment = fs.readFileSync('./comment.txt', 'utf-8');
+    res.send(comment);
 });
 
 app.listen(4000, () => console.log('start server'));
